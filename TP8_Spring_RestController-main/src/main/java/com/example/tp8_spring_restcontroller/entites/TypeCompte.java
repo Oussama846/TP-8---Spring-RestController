75f5e12aa30d6d003bdc7effddde8887b062cb50
Oussama846 <oussamadarradirma@gmail.com>
2025-11-13 17:41:42 +0100

    Delete TP8_Spring_RestController-main directory

--- TP8_Spring_RestController-main/src/main/java/com/example/tp8_spring_restcontroller/entites/TypeCompte.java
+++
@@ -1,5 +0,0 @@
-package com.example.tp8_spring_restcontroller.entites;
-
-public enum TypeCompte {
-    COURANT, EPARGNE
-}
--- TP8_Spring_RestController-main/src/main/java/com/example/tp8_spring_restcontroller/repositories/CompteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tp8_spring_restcontroller.repositories;
-
-import com.example.tp8_spring_restcontroller.entites.Compte;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompteRepository extends JpaRepository<Compte, Long> {
-}
